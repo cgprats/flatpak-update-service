a22188a5a84e7be295cfb1b085ae8a21f05d05fe
Christopher Prats <christopherprats@gmail.com>
2021-03-01 12:48:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "Copying Service Units"
 sudo cp -v flatpak-update.service /etc/systemd/system/flatpak-update.service
-sudo cp -v flatpak-update.timer /etc/systemd/system/flatpak-update.timer
 echo "Reloading Systemd Daemon"
 sudo systemctl daemon-reload
 echo "Enabling Services"
 sudo systemctl enable flatpak-update.service
-sudo systemctl enable flatpak-update.timer
 sudo systemctl start flatpak-update
